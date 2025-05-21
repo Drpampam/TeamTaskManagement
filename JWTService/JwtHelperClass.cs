18f716c0b4e2cb5cba33051e254647de26037d38
Drpampam <johnomotoye@gmail.com>
2025-05-21 13:20:39 +0100

    added a jwt service not yet used

---
+++ JWTService/JwtHelperClass.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TeamTaskManagement.API.Interfaces;
+using TeamTaskManagement.API.Models;
+
+namespace TeamTaskManagement.API.JWTService
+{
+    public class JwtHelperClass
+    {
+        private readonly IConfiguration _configuration;
+        public JwtHelperClass(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateJwt(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
